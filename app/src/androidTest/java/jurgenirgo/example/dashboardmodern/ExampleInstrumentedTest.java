cfa4df15e83dd8b4ef8727b045fa36634d601717
Jurgen <jurgenirgof@gmail.com>
2019-06-29 09:57:59 +0700

    Initial commit

---
+++ app/src/androidTest/java/jurgenirgo/example/dashboardmodern/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package jurgenirgo.example.dashboardmodern;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("jurgenirgo.example.dashboardmodern", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/jurgenirgo/example/dashboardmodern/MainActivity.java
@@ -0,0 +1,13 @@
+package jurgenirgo.example.dashboardmodern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
